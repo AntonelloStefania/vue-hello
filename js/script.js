3cfa0dd719ade2175761ec52dea9348ee1b8f251
AntonelloStefania <antonelloste94@gmail.com>
2023-06-07 14:48:02 +0200

    first commit

---
+++ js/script.js
@@ -0,0 +1,25 @@
+const {createApp} = Vue;
+
+createApp({
+    data() {
+        return {
+            title:"<h1>Crea la tua cartolina digitale</h1>",
+            d_flex_center: "d-flex-content-items-center",
+            img:'<img src="https://picsum.photos/390/300" alt="image">',
+            dear:'',
+            main_text: '',
+            date : '',
+            from:'',
+            border:'brdr-bottom'
+        }
+    },
+    methods: {
+      realDate(){
+        const realDate= new Date();
+        this.date = realDate.toUTCString().slice(5, 16);
+      }
+    },
+    mounted() {
+        this.realDate(); 
+      },
+}).mount("#app")

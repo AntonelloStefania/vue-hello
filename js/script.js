2d4edd01b8cdd636fa1ad5922a0909de4658e84c
AntonelloStefania <antonelloste94@gmail.com>
2023-06-07 17:39:58 +0200

    [add] success email sent

--- js/script.js
+++ js/script.js
@@ -11,7 +11,8 @@ createApp({
             date : '',
             from:'',
             border:'brdr-bottom',
-            font:'f-family'
+            font:'f-family',
+            email:'',
         }
     },
     methods: {
@@ -27,6 +28,19 @@ createApp({
             console.log(number)
             
             this.img= random_img
+       },
+       submitButton(){
+        let alert= document.getElementById("alert");
+        alert.classList.add("appear");
+       },
+       closeAlert(){
+        let alert= document.getElementById("alert");
+        let close = document.getElementById("close-alert");
+        alert.classList.remove("appear");
+        this.email='';
+        this.dear='';
+        this.main_text='';
+        this.from=''
        }
     },
     mounted() {
